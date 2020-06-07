7de86db622dd6cf95a21f2e96e0378243b37ee8a
HenriU <rhino888@gmail.com>
2020-06-07 21:47:23 +0200

    Doing postgres tutorial

--- posgresql1/user.py
+++ posgresql1/user.py
@@ -1,4 +1,5 @@
-from posgresql1.database import ConnectionFromPool
+from posgresql1.database import CursorFromConnectionFromPool
+
 
 class User:
     def __init__(self, email, first_name, last_name, id):
@@ -12,17 +13,15 @@ class User:
 
     def save_to_db(self):
         """This module will save the user to the database"""
-        with ConnectionFromPool() as connection:
-            with connection.cursor() as cursor:
-                cursor.execute('INSERT INTO users(email, first_name, last_name) VALUES(%s, %s, %s)',
-                               (self.email, self.first_name, self.last_name))
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('INSERT INTO users(email, first_name, last_name) VALUES(%s, %s, %s)',
+                           (self.email, self.first_name, self.last_name))
 
     @classmethod
     def load_from_db_by_email(cls, email):
         """This module loads a user from the database, using the email address."""
-        with ConnectionFromPool() as connection:
-            with connection.cursor() as cursor:
-                cursor.execute('SELECT * FROM users WHERE email=%s',
-                                   (email,))
-                user_data = cursor.fetchone()
-                return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('SELECT * FROM users WHERE email=%s',
+                           (email,))
+            user_data = cursor.fetchone()
+            return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
