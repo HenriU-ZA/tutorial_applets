495d9b81b85b257840ebdf9ea1c084a2a2b1d733
HenriU <rhino888@gmail.com>
2020-06-10 20:58:55 +0200

    Adding flask

---
+++ twitter_login/constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'JiUVKON5Xcll8s1abKbcj5ZxP'
+CONSUMER_SECRET = 'kgr8OjIIZm9VtQNRzlUS6ZsvLvq390bWQd3wj0x3XkIDMb6hGW'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
